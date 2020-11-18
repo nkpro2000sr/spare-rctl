bec2dda641bf3e10b28b14d6bbb98156202ace5e
NAVEEN S R <srnaveen2k@yahoo.com>
2020-11-19 01:50:08 +0530

    Update spareRctl.sh

--- spareRctl.sh
+++ spareRctl.sh
@@ -10,24 +10,27 @@ for x in $(cat /proc/cmdline); do
         --novnc) # 6080
                 /usr/bin/novnc &>> sparerctl.log &
                 ;&
-        --vnc) # 5900
+        --vnc*) # 5900
+                password=${x#--vnc:} # --vnc:passwd
                 /usr/bin/x11vnc -xkb -noxrecord -noxfixes -noxdamage \
                                 -repeat -forever -shared \
                                 -auth $(ps wwwwaux | grep auth | grep root | head -n 1 | awk '{ print $15 }') \
-                                -passwd spareRctl.sh &>> sparerctl.log &
+                                -passwd ${password} &>> sparerctl.log &
                 ;;&
         
-        --wifi)
+        --wifi*)
+                y=${x#--wifi:} # --wifi:ssid:passwd
                 false
                 while [[ $? != 0 ]]; do
                     sleep 0.3
                     /usr/bin/nmcli device wifi rescan >> sparerctl.log
                     sleep 0.3
-                    /usr/bin/nmcli device wifi connect spareRctl password spareRctl.sh >> sparerctl.log
+                    /usr/bin/nmcli device wifi connect ${y%%:*} password ${y#*:} >> sparerctl.log
                 done
                 ;;
-        --ap)
-                /usr/bin/nmcli device wifi hotspot ssid spareRctl password spareRctl.sh &>> sparerctl.log
+        --ap*)
+                y=${x#--ap:} # --ap:ssid:passwd
+                /usr/bin/nmcli device wifi hotspot ssid ${y%%:*} password ${y#*:} &>> sparerctl.log
                 ;;
     esac
 done
@@ -44,22 +47,3 @@ done
 if [[ $LOOP == 1 ]]; then
      while true; do sleep 10; done
 fi
-
-## /usr/lib/systemd/system/sparerctl.service
-# [Unit]
-# Description=spare remote control to access at the time of problem
-# Wants=network-online.target
-# After=network.target network-online.target
-# Wants=sshdgenkeys.service
-# After=sshdgenkeys.service
-# Requires=graphical.target
-# #After=graphical.target
-# 
-# [Service]
-# User=root
-# WorkingDirectory=/root
-# ExecStart=/usr/bin/bash /opt/spareRctl.sh
-# KillMode=mixed
-# 
-# [Install]
-# WantedBy=multi-user.target
